7d21b16cf053a3c918a7836e027fae1e68d9638c
mohankrishna2006 <mohanmasarapu@gmail.com>
2025-07-19 10:16:28 +0530

    Add files via upload

---
+++ parse_url.js
@@ -0,0 +1,10 @@
+const url =require('url');
+
+const sampleUrl ='https://www.amazon.in/'
+const parsedUrl=new URL(sampleUrl);
+
+console.log("protocol :",parsedUrl.protocol);
+console.log("Hostname :",parsedUrl.hostname);
+console.log("port :",parsedUrl.port);
+console.log("pathname :",parsedUrl.pathname);
+console.log("search :",parsedUrl.search);
